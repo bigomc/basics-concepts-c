492c74ae5c88260d7a335dee982e1c1858ceb883
Rodrigo Moreno <bigomc@gmail.com>
2024-02-11 20:01:21 -0600

    Solution

--- main.c
+++ main.c
@@ -19,17 +19,66 @@ void print(struct node *list) {
 int push_front(struct node **list, int value) {
     int status = -1;
     
+    struct node *new_element = malloc(sizeof(struct node));
+    if(new_element != NULL) {
+        new_element -> data = value;
+        new_element -> next = NULL;
+        
+        if(*list == NULL) {
+            *list = new_element;
+        } else {
+            new_element -> next = *list;
+            *list = new_element;
+        }
+        
+        status = 0;
+    }
+    
     return status;
 }
 
 int pop_front(struct node **list, int *value) {
     int status = -1;
     
+    if(*list != NULL) {
+        struct node *tmp = *list;
+        *value = tmp -> data;
+        *list = tmp -> next;
+        free(tmp);
+        
+        status = 0;
+    }
+    
     return status;
 }
 
 int remove_at(struct node **list, int index) {
     int status = -1;
+    
+    if(*list != NULL) {
+        int length = 0;
+        struct node *tmp = *list;
+        while(tmp != NULL) {
+            length++;
+            tmp = tmp -> next;
+        }
+
+        int limit = length - index;
+        tmp = * list;
+        struct node *prev = NULL;
+        while(limit > 0) {
+            limit--;
+            prev = tmp;
+            tmp = tmp -> next;
+        }
+        
+        if(prev == NULL) {
+            *list = tmp -> next;
+        } else {
+            prev -> next = tmp -> next;
+        }
+        free(tmp);
+    }
     
     return status;
 }
